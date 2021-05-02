f8cacc8703414e4ef25de1b1401f0a83bc4f667b
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2021-05-02 17:21:38 +0900

    Build generate grpc environment

---
+++ proto/scripts/generate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -xe
+
+SERVER_OUTPUT_DIR=server/generated
+CLIENT_OUTPUT_DIR=client/generated
+
+protoc --version
+protoc --proto_path=proto `find proto -iname "*.proto"` \
+  --go_out=${SERVER_OUTPUT_DIR} \
+  --go-grpc_out=${SERVER_OUTPUT_DIR} \
+  --js_out=import_style=commonjs:${CLIENT_OUTPUT_DIR} \
+  --grpc-web_out=import_style=typescript,mode=grpcwebtext:${CLIENT_OUTPUT_DIR}
