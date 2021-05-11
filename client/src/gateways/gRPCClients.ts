646094385b5246859662e42d47664e68a10e78d0
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2021-05-11 13:32:59 +0900

    Add prod grpc url

--- client/src/gateways/gRPCClients.ts
+++ client/src/gateways/gRPCClients.ts
@@ -8,8 +8,16 @@ export type GRPCClients = {
   recordingServiceClient: RecordingServiceClient
 }
 
+// 開発
+// export const gRPCClients = {
+//   userServiceClient: new UserServiceClient(`http://localhost:8080`),
+//   taskServiceClient: new TaskServiceClient(`http://localhost:8080`),
+//   recordingServiceClient: new RecordingServiceClient(`http://localhost:8080`)
+// }
+
+// 本番
 export const gRPCClients = {
-  userServiceClient: new UserServiceClient(`http://localhost:8080`),
-  taskServiceClient: new TaskServiceClient(`http://localhost:8080`),
-  recordingServiceClient: new RecordingServiceClient(`http://localhost:8080`)
+  userServiceClient: new UserServiceClient(`http://speaking-exam-lb-327276559.ap-northeast-1.elb.amazonaws.com`),
+  taskServiceClient: new TaskServiceClient(`http://speaking-exam-lb-327276559.ap-northeast-1.elb.amazonaws.com`),
+  recordingServiceClient: new RecordingServiceClient(`http://speaking-exam-lb-327276559.ap-northeast-1.elb.amazonaws.com`)
 }
