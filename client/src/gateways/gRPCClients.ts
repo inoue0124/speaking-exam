293ba12d2d8de7f38cd6004972f9cfa745196a3d
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2021-05-20 22:59:10 +0900

    Fix api endpoint

--- client/src/gateways/gRPCClients.ts
+++ client/src/gateways/gRPCClients.ts
@@ -23,5 +23,5 @@ export const gRPCClients = {
   userServiceClient: new UserServiceClient(`https://api-speaking.komabastar.com`),
   taskServiceClient: new TaskServiceClient(`https://api-speaking.komabastar.com`),
   examServiceClient: new ExamServiceClient(`https://api-speaking.komabastar.com`),
-  recordingServiceClient: new RecordingServiceClient(`http:s//api-speaking.komabastar.com`)
+  recordingServiceClient: new RecordingServiceClient(`https://api-speaking.komabastar.com`)
 }
